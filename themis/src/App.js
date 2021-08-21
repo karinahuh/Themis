af8a1c48f68fc954907f770871f8928124bdc89f
Karina <karinahuh98@gmail.com>
2021-08-21 18:36:07 -0300

    Start

---
+++ themis/src/App.js
@@ -0,0 +1,23 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="themis.png"/>
+        <p>
+        Assistência Jurídica para Mulheres.<br></br>
+        ENCONTRE ADVOGADAS • TIRE DÚVIDAS<br></br>
+
+        <button>Começar</button>
+        <br></br>
+        <button>Sobre Nós</button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
