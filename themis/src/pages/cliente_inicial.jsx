9a28ec7db2087fec43e41749a4b7133f2f926932
Karina <karinahuh98@gmail.com>
2021-08-21 22:19:15 -0300

    Pages 2

---
+++ themis/src/pages/cliente_inicial.jsx
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import React from 'react';
+
+function cliente_inicial() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="themis.png"/>
+        <p>
+          EXPLORAR<br></br>
+          <button>Advogadas</button><br></br>
+          <button>Perguntar</button><br></br>
+          <button>Termos Jurídicos</button><br></br>
+        </p>
+      </header>
+      </div>
+  );
+}
+
+export default cliente_inicial;
--- themis/src/pages/routes.jsx
+++ themis/src/pages/routes.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import index from '.';
-import Cadastro from './cadastro'
-import CadastroAdvogada from './cadastro_advogada'
-import CadastroCliente from './cadastro_cliente'
-import Login from './login'
+import Login from './login';
+import Cadastro from './cadastro';
+import CadastroAdvogada from './cadastro_advogada';
+import CadastroCliente from './cadastro_cliente';
+import ClienteIncial from './cliente_inicial';
+import TermosJuridicos from './termos_juridicos';
+import Advogadas from './advogadas';
+import Perguntas from './perguntas';
 
 function Routes() {
     return(
@@ -14,6 +18,10 @@ function Routes() {
             <Route path="/cadastro" exact component={Cadastro} />
             <Route path="/cadastro_advogada" exact component={CadastroAdvogada} />
             <Route path="/cadastro_cliente" exact component={CadastroCliente} />
+            <Route path="/cliente_inicial" exact component={ClienteIncial} />
+            <Route path="/termos_juridicos" exact component={TermosJuridicos} />
+            <Route path="/advogadas" exact component={Advogadas} />
+            <Route path="/perguntas" exact component={Perguntas} />
         </BrowserRouter>
     );
 }
